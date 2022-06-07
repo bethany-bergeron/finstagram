8b10ebc2af2ba102c743f7323c69afb505300c60
Bethany Bergeron <msbethanybouchard@gmail.com>
2022-06-07 03:24:18 +0000

    W03D1: adding hashes and changing to .erb files

--- app/actions.rb
+++ app/actions.rb
@@ -1,25 +1,3 @@
-# W2D2: Introducing Control Flow & if Statements to print humanized string of time
-#
-# get '/' do
-#   username = "sharky_j"
-#   avatar_url = "http://naserca.com/images/sharky_j.jpg"
-#   photo_url = "http://naserca.com/images/shark.jpg"
-#   time_ago_in_minutes = 5
-#   like_count = 0
-#   comment_count = 1
-#   comments = [
-#     "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
-#   ]
-
-#   if time_ago_in_minutes >= 60
-#     "#{time_ago_in_minutes / 60} hours ago"
-#   else
-#     "#{time_ago_in_minutes} minutes ago"
-#   end
-# end
-
-# W2D2: Defining a Method to print humanized string of time
-
 def humanized_time_ago(minute_num)
   if minute_num >= 60
     "#{minute_num / 60} hours ago"
@@ -28,46 +6,21 @@ def humanized_time_ago(minute_num)
   end
 end
 
-# Using variables
-#
-# get '/' do
-#   username = "sharky_j"
-#   avatar_url = "http://naserca.com/images/sharky_j.jpg"
-#   photo_url = "http://naserca.com/images/shark.jpg"
-#   time_ago_in_minutes = 15
-#   like_count = 0
-#   comment_count = 1
-#   comments = [
-#     "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
-#   ]
-
-#   humanized_time_ago(time_ago_in_minutes)
-
-# end
-
-# Using hashes (finstagram post is our variable, which bundles the hashes)
-
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
-    # putting the value that the method returns into a hash
     humanized_time_ago: humanized_time_ago(15),
     like_count: 0,
     comment_count: 1,
-# previously - comment array, not as simplified with a username and text hash
-#    comments: [
-#      "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
-#    ]
-    # now - comments hash with an array of one element which has two hashes
     comments: [{
       username: "sharky_j",
       text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -80,7 +33,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -92,7 +45,8 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  # putting hashes into a single array, where to_s turns it into a string
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
-erb(:index)
+
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+  
+  erb(:index)
 end
